da0922ae04f64c62af68e617ba67b0fb0e3e5f7c
apurva mayank <apurva@weboniselab.com>
2013-05-22 15:57:54 +0530

    boostrap added in basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BootstrapDemo::Application.config.secret_token = '7e1296d314d077a19d39010dda182ed0498af9668c940a75710aac15a884e6468f3036075535c20e6fcdfdde74719eb45e81d5b92f19589799e56162caa40125'
